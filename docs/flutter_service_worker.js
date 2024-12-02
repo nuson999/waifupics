d75a792ab0574c1e7d4e3a9daebab6f1ddf60bd7
nuson999 <eungangku@gmail.com>
2024-12-02 23:06:19 +0100

    Update service worker version and resource hashes in bootstrap and service worker files

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "db3ee4a31c664a1fe48596d0cb1dde0a",
+"flutter_bootstrap.js": "7581b376823e53dbe9e976dfcec95de9",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "ee9351269d4c4a41a620e0c4ede6a21c",
 "/": "ee9351269d4c4a41a620e0c4ede6a21c",
-"main.dart.js": "662eee9c264a7d4c2669a1962535d0ad",
+"main.dart.js": "b9dc58da47f7c5e4ea8c03b96af93ff8",
 "manifest.json": "41e75fe91dd94455aeb10e9eb65dbd87",
 "version.json": "dee97b27f532ff7ea484e43433c03d5c"};
 // The application shell files that are downloaded before a service worker can
